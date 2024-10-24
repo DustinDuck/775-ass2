661eab8e0b42786c893318cf7571cf0b33d7a12c
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-10-23 22:27:53 -0700

    first commit

--- FatTree.py
+++ FatTree.py
@@ -15,39 +15,42 @@ class FatTree(Topo):
         Agg_L2 = pod*pod//2 
         Acc_L1 = Agg_L2
 
-        c = [] 
-        a = [] 
-        e = [] 
+        c = [] # core layer switches
+        a = [] # aggregation layer switches
+        e = [] # edge layer switches
 
-        # Adding dpid
+        # Adding core layer switches with unique dpid
         for i in range (Core_L3):
             c_sw = self.addSwitch('L3_C-S{}'.format(i+1), dpid='{:016x}'.format(i+1))
             c.append(c_sw)
              
-        # Adding dpid
+        # Adding aggregation layer switches with unique dpid
         for i in range (Agg_L2):
             a_sw = self.addSwitch('L2_Agg-S{}'.format(i+1), dpid='{:016x}'.format(Core_L3 + i + 1))
             a.append(a_sw)    
             
-        # Adding dpid
+        # Adding edge layer switches with unique dpid
         for i in range (Acc_L1):
             e_sw = self.addSwitch('L1_Acc-S{}'.format(i+1), dpid='{:016x}'.format(Core_L3 + Agg_L2 + i + 1))
             e.append(e_sw)      
-            
+
+        # Creating links between core layer and aggregation layer
         for i in range(Core_L3):
             c_sw = c[i]
             start = i // (pod // 2)
             for j in range(pod):
-                self.addLink(c_sw, a[start + j * (pod // 2)], bw=8, delay='3ms')
+                self.addLink(c_sw, a[start + j * (pod // 2)], cls=TCLink, bw=8, delay='3ms')
 
+        # Creating links between aggregation layer and edge layer
         for i in range(Agg_L2):
             group = i // (pod // 2)
             for j in range(pod // 2):
-                self.addLink(a[i], e[group * (pod // 2) + j], bw=8, delay='3ms')
+                self.addLink(a[i], e[group * (pod // 2) + j], cls=TCLink, bw=8, delay='3ms')
 
+        # Creating links between edge layer and hosts and creating hosts
         for i in range(Acc_L1):
             for j in range(4):
                 hs = self.addHost('h{}'.format(i * 4 + j + 1))
-                self.addLink(e[i], hs, bw=8, delay='3ms')
+                self.addLink(e[i], hs, cls=TCLink, bw=8, delay='3ms')
 
 topos = {'fattree': (lambda: FatTree())}
