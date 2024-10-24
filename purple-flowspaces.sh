1adce062f547373ed19a6a2273d77bc97df77c10
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-10-24 00:18:12 -0700

    first commit

--- purple-flowspaces.sh
+++ purple-flowspaces.sh
@@ -1,7 +1,25 @@
-# h15 -> h16
-# fvctl -n add-flowspace bfs1 31 1 in_port=7,nw_src=10.0.0.15,nw_dst=10.0.0.16 blue=7
-# fvctl -n add-flowspace bfs2 31 1 in_port=8,nw_src=10.0.0.16,nw_dst=10.0.0.15 blue=7
+# h15 <-> h16
+# L1_Acc-S4
 fvctl -n add-flowspace pfs1 0000000000000034 1 in_port=7,nw_src=10.0.0.15,nw_dst=10.0.0.16 purple=7
 fvctl -n add-flowspace pfs2 0000000000000034 1 in_port=8,nw_src=10.0.0.16,nw_dst=10.0.0.15 purple=7
+
+# h15 <-> h17
+# L1_Acc-S4
+fvctl -n add-flowspace pfs3 0000000000000034 1 in_port=7,nw_src=10.0.0.15,nw_dst=10.0.0.17 purple=7
+fvctl -n add-flowspace pfs4 0000000000000034 1 in_port=2,nw_src=10.0.0.17,nw_dst=10.0.0.15 purple=7
+# L1_Acc-S5
+fvctl -n add-flowspace pfs5 0000000000000035 1 in_port=2,nw_src=10.0.0.15,nw_dst=10.0.0.17 purple=7
+fvctl -n add-flowspace pfs6 0000000000000035 1 in_port=5,nw_src=10.0.0.17,nw_dst=10.0.0.15 purple=7
+# L2_Agg-S2
+fvctl -n add-flowspace pfs7 0000000000000012 1 in_port=8,nw_src=10.0.0.15,nw_dst=10.0.0.17 purple=7
+fvctl -n add-flowspace pfs8 0000000000000012 1 in_port=2,nw_src=10.0.0.17,nw_dst=10.0.0.15 purple=7
+# L2_Agg-S6
+fvctl -n add-flowspace pfs9 0000000000000016 1 in_port=2,nw_src=10.0.0.15,nw_dst=10.0.0.17 purple=7
+fvctl -n add-flowspace pfs10 0000000000000016 1 in_port=5,nw_src=10.0.0.17,nw_dst=10.0.0.15 purple=7
+# L3_C-S6
+fvctl -n add-flowspace pfs99 0000000000000006 1 in_port=1,nw_src=10.0.0.15,nw_dst=10.0.0.17 purple=7
+fvctl -n add-flowspace pfs100 0000000000000006 1 in_port=2,nw_src=10.0.0.17,nw_dst=10.0.0.15 purple=7
+
+
 
 # fvctl -n add-flowspace bfs2 0000000000000031 1 in_port=3,nw_src=10.0.0.15,nw_dst=10.0.0.2 blue=7
