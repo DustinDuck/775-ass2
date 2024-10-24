72a6faee20ccc154b4d78c46783e1e32d27237a4
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-10-24 01:03:33 -0700

    first commit

--- remove_all_fs.sh
+++ remove_all_fs.sh
@@ -1,3 +1,3 @@
-for flowspace in $(sudo fvctl -n list-flowspace | grep '"name"' | awk -F'"' '{print $4}'); do
+for flowspace in $(sudo fvctl -n list-flowspace | grep '"name"' | awk -F'"' '{print $6}'); do
     sudo fvctl -n remove-flowspace $flowspace
 done
