7163ada47e6c335f8bb96e6c62d39546fc37a816
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-10-23 22:51:32 -0700

    first commit

---
+++ blue-flowspaces.sh
@@ -0,0 +1,5 @@
+# h15 -> h16
+fvctl -n add-flowspace bfs1 0000000000000031 1 in_port=3,nw_src=10.0.0.15,nw_dst=10.0.0.16 blue=7
+fvctl -n add-flowspace bfs1 0000000000000031 1 in_port=4,nw_src=10.0.0.16,nw_dst=10.0.0.15 blue=7
+
+# fvctl -n add-flowspace bfs2 0000000000000031 1 in_port=3,nw_src=10.0.0.15,nw_dst=10.0.0.2 blue=7
