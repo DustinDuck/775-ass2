8d131855bda0f78a65e2c433416a10a2347d852b
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-10-23 23:20:23 -0700

    first commit

--- purple-flowspaces.sh
+++ purple-flowspaces.sh
@@ -1,7 +1,7 @@
 # h15 -> h16
 # fvctl -n add-flowspace bfs1 31 1 in_port=7,nw_src=10.0.0.15,nw_dst=10.0.0.16 blue=7
 # fvctl -n add-flowspace bfs2 31 1 in_port=8,nw_src=10.0.0.16,nw_dst=10.0.0.15 blue=7
-fvctl -n add-flowspace bfs1 31 1 in_port=7 blue=7
-fvctl -n add-flowspace bfs2 31 1 in_port=8 blue=7
+fvctl -n add-flowspace pfs1 31 1 in_port=7 purple=7
+fvctl -n add-flowspace pfs2 31 1 in_port=8 purple=7
 
 # fvctl -n add-flowspace bfs2 0000000000000031 1 in_port=3,nw_src=10.0.0.15,nw_dst=10.0.0.2 blue=7
